36fd38afd7cc692151ed569ed5b00059fc073298
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-08-09 13:25:35 -0600

    Rails Backend Up

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# Message.create(text: 'Hello World! 1')
+# Message.create(text: 'Hello World! 2')
+# Message.create(text: 'Hello World! 3')
+# Message.create(text: 'Hello World! 4')
+# Message.create(text: 'Hello World! 5')
